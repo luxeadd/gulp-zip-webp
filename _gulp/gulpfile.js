b80cef2310ea9d7fe0fb0e91d4ef82a33487b219
sassy <luxeadd@gmail.com>
2024-05-24 21:30:38 +0900

    画像の追加と圧縮、Gulpタスクの追加

---
+++ _gulp/gulpfile.js
@@ -0,0 +1,97 @@
+const { src, dest, watch, series, parallel } = require("gulp"); // Gulpの基本関数をインポート
+const imageminSvgo = require("imagemin-svgo"); // SVGを最適化するためのモジュール
+const imagemin = require("gulp-imagemin"); // 画像を最適化するためのモジュール
+const imageminMozjpeg = require("imagemin-mozjpeg"); // JPEGを最適化するためのモジュール
+const imageminPngquant = require("imagemin-pngquant"); // PNGを最適化するためのモジュール
+const changed = require("gulp-changed"); // 変更されたファイルのみを対象にするためのモジュール
+const del = require("del"); // ファイルやディレクトリを削除するためのモジュール
+const webp = require("gulp-webp"); //webp変換
+const rename = require("gulp-rename"); //ファイル名変更
+const replace = require("gulp-replace"); // 文字列や正規表現による置換
+const zip = require("gulp-zip");
+
+//zip変換後の名前を設定
+const zipFailName = "archive";
+
+// 読み込み元
+const srcPath = {
+  img: "../images/**/*",
+};
+const srcZipPath = {
+  zip: "../zip/**/*",
+};
+// 出力先
+let destPath  = {
+  img: "../dist-images/",
+};
+let destZipPath  = {
+  zip: "../dist-zip/",
+};
+
+// 画像圧縮
+const imgImagemin = () => {
+  // 画像ファイルを指定
+  return (
+    src(srcPath.img)
+      //変更があった画像のみ処理対象に
+      .pipe(changed(destPath.img))
+      // 画像を圧縮
+      .pipe(
+        imagemin(
+          [
+            // JPEG画像の圧縮設定
+            imageminMozjpeg({
+              quality: 80, // 圧縮品質（0〜100）
+            }),
+            // PNG画像の圧縮設定
+            imageminPngquant(),
+            // SVG画像の圧縮設定
+            imageminSvgo({
+              plugins: [
+                {
+                  removeViewbox: false, // viewBox属性を削除しない
+                },
+              ],
+            }),
+          ],
+          {
+            verbose: true, // 圧縮情報を表示
+          }
+        )
+      )
+      //変換前の拡張子での圧縮画像が必要な場合
+      // .pipe(dest(destPath.img))
+      .pipe(webp())
+      .pipe(dest(destPath.img))
+  );
+};
+
+// ファイルの削除
+const clean = () => {
+  return del(destPath.img, { force: true });
+};
+const cleanZip = () => {
+  return del(destZipPath.zip, { force: true });
+};
+
+// ファイルの圧縮
+const archive = () => {
+  const now = new Date();
+  const year = now.getFullYear().toString().slice(-2);
+  const month = ("0" + (now.getMonth() + 1)).slice(-2);
+  const day = now.getDate().toString().slice(-2);
+
+  const yymmdd = year + month + day;
+
+  return src(srcZipPath.zip)
+    .pipe(zip(`${zipFailName}_${yymmdd}.zip`))
+    .pipe(dest(destZipPath.zip));
+};
+
+// exports.default = series(
+//   series(cssSass, jsBabel, imgImagemin, ejsCompile),
+//   parallel(watchFiles, browserSyncFunc)
+// );
+
+exports.webp = series(clean, imgImagemin);
+exports.zip = series(cleanZip, archive);
